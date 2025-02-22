6ecfd1bb619bf7367bf1041850f1ba2e5f6477aa
IvanGu <gusagusa@gmail.com>
2025-02-22 14:53:05 +0300

    u0

---
+++ main.py
@@ -0,0 +1,147 @@
+"""MANDELBROT"""
+import os
+import threading
+# import cmath
+# import random as rnd
+import pygame as pg
+import numpy as np
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+
+COLOR_WHITE = (255, 255, 255)
+COLOR_BLACK = (0, 0, 0)
+COLOR_BACKGROUND = COLOR_WHITE
+
+SCREEN_WIDTH = 900
+SCREEN_HEIGHT = 600
+SCREEN_FPS = 30
+
+MANDELBROT_MAX_CHECK_ITER = 100
+
+field_topleft = complex(-2.0, 2.0)
+field_scale = 4.0/SCREEN_HEIGHT
+
+running = True
+need_redraw = False
+field_np: np.array = None
+th: threading.Thread = None
+stop_event = threading.Event()
+stop_event.set()
+
+def recalculate() -> None:
+    global stop_event
+    global th
+    global field_np
+    global need_redraw
+
+    print("recalculate STARTED")
+    stop_event.set()
+
+    field_np = np.full((SCREEN_WIDTH, SCREEN_HEIGHT),
+                       MANDELBROT_MAX_CHECK_ITER+1, dtype=int)
+    stop_event = threading.Event()
+    th = threading.Thread(target=calculate_field,
+                          daemon=True, args=(stop_event, field_np))
+    th.start()
+    need_redraw = True
+
+
+def get_mandelbrot_rate(c: complex, max_check_iter: int = 100) -> tuple[bool, int]:
+    z = complex(0.0, 0.0)
+    for i in range(1, max_check_iter):
+        z = z**2 + c
+        if abs(z) > 2.0:
+            return (False, i)
+    return (True, max_check_iter)
+
+
+def handle_events():
+    global field_topleft
+    global field_scale
+
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            return False
+
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_ESCAPE:
+                return False
+            if event.key == pg.K_HOME:
+                field_topleft = complex(-2.0, 2.0)
+                field_scale = 4.0/SCREEN_HEIGHT
+
+    #     if event.type != pg.MOUSEMOTION:
+    #         # print(f"{draw_scene.left} {draw_scene.top}")
+    #         pass
+
+    # keys = pg.key.get_pressed()
+    # if keys[pg.K_LEFT]:
+    #     draw_scene.left -= 1
+    # elif keys[pg.K_RIGHT]:
+    #     draw_scene.left += 1
+    # elif keys[pg.K_UP]:
+    #     draw_scene.top -= 1
+    # elif keys[pg.K_DOWN]:
+    #     draw_scene.top += 1
+
+    return True
+
+
+def calculate_field(stop_event, arr: np.array):
+    global need_redraw
+    print("calculate_field STARTED")
+    for i in range(SCREEN_WIDTH):
+        for j in range(SCREEN_HEIGHT):
+            if (not running) or stop_event.is_set():
+                return
+            c = complex(field_topleft.real+field_scale*i,
+                        field_topleft.imag-field_scale*j)
+            _, k = get_mandelbrot_rate(c, MANDELBROT_MAX_CHECK_ITER)
+            arr[i, j] = k
+            need_redraw = True
+            # print(f'{c} - {b}')
+    print("calculate_field DONE")
+
+
+def draw_scene(sc: pg.Surface):
+    global need_redraw
+    if need_redraw:
+        need_redraw = False
+        sc.fill(COLOR_BACKGROUND)
+        for i in range(SCREEN_WIDTH):
+            for j in range(SCREEN_HEIGHT):
+                if field_np[i, j] >= MANDELBROT_MAX_CHECK_ITER:
+                    sc.set_at((i, j), COLOR_BLACK)
+        pg.display.update()
+
+
+def set_caption():
+    pg.display.set_caption(
+        f'Field: {field_topleft} - '
+        f'{complex(
+            field_topleft.real+field_scale*SCREEN_WIDTH,
+            field_topleft.imag-field_scale*SCREEN_HEIGHT)} Scale: {field_scale}')
+
+
+def main():
+    global running
+    global th
+
+    pg.init()
+    sc = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    set_caption()
+
+    clock = pg.time.Clock()
+    recalculate()
+
+    while running:
+        clock.tick(SCREEN_FPS)
+        running = handle_events()
+        draw_scene(sc)
+        if not running:
+            stop_event.set()
+    pg.quit()
+
+
+if __name__ == '__main__':
+    main()
